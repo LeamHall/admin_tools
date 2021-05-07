e456c1bb9c31b35596d2f61373cb27642dab43dd
Leam Hall <leamhall@gmail.com>
2021-05-07 07:56:09 -0400

    some cleanup of find_copies.py, and breaking up methods.

--- find_copies.py
+++ find_copies.py
@@ -6,7 +6,6 @@
 # desc:       Tool to help clean up old versions of files.
 
 from pathlib import Path
-import re
 
 def build_set_from_file(file):
   ''' 
@@ -21,33 +20,56 @@ def build_set_from_file(file):
       line = line.strip()
       my_set.add(line)
   return my_set
-   
-def build_dir_set(my_list, exclude_dirs):
-  my_set  = set()
-  for item in my_list:
-    path = Path(item)
-    parent = str(path.parent)
-    my_set.add(parent)
-  my_new_set = set()
-  for exclude_dir in exclude_dirs:
-    for parent in my_set:
-      if re.match(exclude_dir, parent):
-        my_new_set.add(parent) 
-  return my_set - my_new_set
+  
+def build_dir_set(dirs):
+  '''
+  Given a list of full file paths, make a set of the parent directory
+    names as strings.
+  '''
+  dir_set = set()
+  for item in dirs:
+    path    = Path(item)
+    parent  = str(path.parent)  
+    dir_set.add(parent)
+  return dir_set
 
-master_data = dict()
+def build_clean_set(full_set, exclude_list, match = 'full'):
+  '''
+  Given a set (or list), and a set/list of items to be excluded,
+    return a set of the original set/list minus the excluded items.
+  Optionally, match the exclusion on the entire string, or the 
+    beginning. If there's a use for it, then endswith, too.
+  '''
 
-exclude_files_file  = 'data/exclude_list.txt'
-exclude_dirs_file   = 'data/excluded_dirs.list'
-seed_file_file      = 'data/seed.list'
-exclude_files       = build_set_from_file(exclude_files_file)
-exclude_dirs        = build_set_from_file(exclude_dirs_file)
-seed_files          = build_set_from_file(seed_file_file)
-seed_dirs           = build_dir_set(seed_files, exclude_dirs)
-print("There are {} files in the excluded set.".format(len(exclude_files)))
-print("There are {} dirs in the exclude set.".format(len(exclude_dirs)))
-print("There are {} dirs in the seed set.".format(len(seed_dirs)))
-for item in sorted(seed_dirs):
-  print(item)
+  exclude_set = set()
+  for exclude in exclude_list:
+    for item in full_set:
+      if ( ( match == 'starts' and item.startswith(exclude) ) or
+          ( match == 'full' and item == exclude ) ):
+        exclude_set.add(item) 
+  return full_set - exclude_set
+
+
+
+if __name__ == '__main__':
+
+  master_data = dict()
+
+  # Need to verify if these files exist. 
+  exclude_files_file  = 'data/exclude_files.list'
+  exclude_dirs_file   = 'data/excluded_dirs.list'
+  seed_file_file      = 'data/seed.list'
+
+  exclude_files       = build_set_from_file(exclude_files_file)
+  exclude_dirs        = build_set_from_file(exclude_dirs_file)
+  seed_files          = build_set_from_file(seed_file_file)
+  seed_dirs           = build_dir_set(seed_files)
+  search_dirs         = build_clean_set(seed_dirs, exclude_dirs, 'starts')
+
+  print("There are {} files in the exclude set.".format(len(exclude_files)))
+  print("There are {} dirs in the exclude set.".format(len(exclude_dirs)))
+  print("There are {} dirs in the seed set.".format(len(seed_dirs)))
+  for item in sorted(search_dirs):
+    print(item)
 
 

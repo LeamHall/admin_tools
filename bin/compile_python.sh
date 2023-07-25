8490e40e222e0c51f84ca8c98b5c39e014156a0e
Leam Hall <leamhall@gmail.com>
2023-07-25 06:09:30 -0500

    enable_test_zipfile64.py removes the blockers for that test.

--- bin/compile_python.sh
+++ bin/compile_python.sh
@@ -21,7 +21,7 @@ language='cpython'
 full_dir="${base_dir}${language}"
 date=`date +%Y%m%d`
 minute=`date +%H%M`
-timestamp="${date}_${time}"
+timestamp="${date}_${minute}"
 scriptname=`basename $0`
 self=`whoami`
 
@@ -49,10 +49,14 @@ fi
 
 # Main build section
 git pull > ../logs/${language}_git_pull_${timestamp}.log 2>&1 
+
+# Change test_zipfile64.py to enable the full test.
+enable_test_zipfile64.py $full_dir
+
 make clean > ../logs/${language}_make_clean_${timestamp}.log 2>&1
 make regen-configure > ../logs/${language}_regen-configure_${timestamp}.log 2>&1
-./configure --with-pydebug > ../logs/${language}_configure${timestamp}.log 2>&1
+./configure --with-pydebug > ../logs/${language}_configure_${timestamp}.log 2>&1
 make -j > ../logs/${language}_make_j_${timestamp}.log 2>&1
 make regen-global-objects > ../logs/${language}_make_regen_global_objects_${timestamp}.log 2>&1
-./python -m test -uall > ../logs/${language}_python_m_test_${timestamp}.log 2>&1
+./python -m test -uall -j0 > ../logs/${language}_python_m_test_${timestamp}.log 2>&1
 make patchcheck > ../logs/${language}_make_patchcheck_${timestamp}.log 2>&1
